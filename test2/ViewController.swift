fe0283398a2599a15e7996813ef846817dc97722
mac <mac@macde-macbook-pro.local>
2018-07-08 15:39:26 +0800

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by mac on 2018/7/8.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
